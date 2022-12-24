6bc6b44345ffd296e98107dc9ee583016b8ee226
Melara <nitrogen1456@gmail.com>
2022-12-24 13:28:33 +0900

    fix

--- esp_robo_test2/data/touch.js
+++ esp_robo_test2/data/touch.js
@@ -2,35 +2,64 @@ var joypad = document.getElementById('joypad');
 var turnleft = document.getElementById('turnleft');
 var turnright = document.getElementById('turnright');
 
+var originx = 0
+var originy = 0
+
+var lastx = 0
+var lasty = 0
+
+var oldXData;
+var nowXData;
+
+var oldYData;
+var nowYData;
+
 joypad.addEventListener('touchstart', function(event) {
     joypad.style.backgroundColor = 'red';
-    $.ajax({url: "/setLED?ox="+event.changedTouches[0].pageX, success: function(result){
-    $("#div1").html(result);}});
-    $.ajax({url: "/setLED?oy="+event.changedTouches[0].pageY, success: function(result){
-    $("#div1").html(result);}});
+    originx = event.changedTouches[0].pageX
+    originy = event.changedTouches[0].pageY
 }, false);
 joypad.addEventListener('touchmove', function(event) {
     event.preventDefault();
-    $.ajax({url: "/setLED?px="+event.changedTouches[0].pageX, success: function(result){
-    $("#div1").html(result);}});
-    $.ajax({url: "/setLED?py="+event.changedTouches[0].pageY, success: function(result){
-    $("#div1").html(result);}});
+    // pageXとかpageYが一定値を超えたとき(10ごと)にのみ送信して頻度を低下させる？
+    // 可能なら処理が間に合う一定時間ごとの送信にしたい(5-6msくらい？)
+
+    
+    // arduino側の処理頻度を低下させる
+    // 最新の値のみをpost状態にしてそれ以外はabortする
+    newx = event.changedTouches[0].pageX
+    newy = event.changedTouches[0].pageY
+
+    oldXData.abort()
+    oldYData.abort();
+
+    oldXData = nowXData;
+    oldYData = nowYData;
+
+    if (Math.abs(newx - lastx) > 16){
+        nowXData = $.ajax({url: "/joyEvent?px="+newx - originx, success: function(result){
+        $("#div1").html(result);}});
+        lastx = newx
+    }
+    if (Math.abs(newy - lasty) > 16){
+        nowYData = $.ajax({url: "/joyEvent?py="+newy - originy, success: function(result){
+        $("#div1").html(result);}});
+        lasty = newy
+    }
 }, false);
 joypad.addEventListener('touchend', function(event) {
     joypad.style.backgroundColor = 'blue';
-    $.ajax({url: "/setLED?ox="+0, success: function(result){
+    originx = 0
+    originy = 0
+    $.ajax({url: "/joyEvent?px=0", success: function(result){
     $("#div1").html(result);}});
-    $.ajax({url: "/setLED?oy="+0, success: function(result){
-    $("#div1").html(result);}});
-    $.ajax({url: "/setLED?px="+0, success: function(result){
-    $("#div1").html(result);}});
-    $.ajax({url: "/setLED?py="+0, success: function(result){
+    $.ajax({url: "/joyEvent?py=0", success: function(result){
     $("#div1").html(result);}});
 }, false);
 
 turnleft.addEventListener('touchstart', function(event) {
     turnleft.style.backgroundColor = 'red';
-    $.ajax({url: "/setLED?l="+1, success: function(result){
+    $.ajax({url: "/leftEvent?l=1", success: function(result){
     $("#div1").html(result);}});
 }, false);
 turnleft.addEventListener('touchmove', function(event) {
@@ -38,13 +67,13 @@ turnleft.addEventListener('touchmove', function(event) {
 }, false);
 turnleft.addEventListener('touchend', function(event) {
     turnleft.style.backgroundColor = 'yellow';
-    $.ajax({url: "/setLED?l="+0, success: function(result){
+    $.ajax({url: "/leftEvent?l=0", success: function(result){
         $("#div1").html(result);}});
 }, false);
 
 turnright.addEventListener('touchstart', function(event) {
     turnright.style.backgroundColor = 'red';
-    $.ajax({url: "/setLED?r="+1, success: function(result){
+    $.ajax({url: "/rightEvent?r=1", success: function(result){
     $("#div1").html(result);}});
 }, false);
 turnright.addEventListener('touchmove', function(event) {
@@ -52,6 +81,6 @@ turnright.addEventListener('touchmove', function(event) {
 }, false);
 turnright.addEventListener('touchend', function(event) {
     turnright.style.backgroundColor = 'orange';
-    $.ajax({url: "/setLED?r="+0, success: function(result){
+    $.ajax({url: "/rightEvent?r=0", success: function(result){
         $("#div1").html(result);}});
 }, false);
--- esp_robo_test2/esp_robo_test2.ino
+++ esp_robo_test2/esp_robo_test2.ino
@@ -5,25 +5,19 @@
 
 const char* htmlfile = "/index.html";
 
-#define RIGHT_REAR_1 5
-#define RIGHT_REAR_2 14
-
-#define RIGHT_FRONT_1 2
-#define RIGHT_FRONT_2 4
-
-#define LEFT_REAR_1 13
-#define LEFT_REAR_2 12
-
-#define LEFT_FRONT_1 16
-#define LEFT_FRONT_2 15
-
-#define COS45 0.707
-
 #define SERIAL true
 
 const char *ssid = "access_point";
 const char *pass = "pasuwaado";
 ESP8266WebServer Server(80);
+
+unsigned long ts;
+unsigned long te;
+
+int RIGHT_REAR[] = {5, 14};
+int RIGHT_FRONT[] = {2, 4};
+int LEFT_REAR[] = {13, 12};
+int LEFT_FRONT[] = {16, 15};
 
 // x方向の原点
 int originX = 0;
@@ -38,58 +32,33 @@ int turnL = 0;
 // 右旋回ボタン
 int turnR = 0;
 
-void handlePWM() {
+void joyStart() {
   if(Server.arg("ox") != NULL)  originX = (Server.arg("ox")).toInt();
   if(Server.arg("oy") != NULL)  originY = (Server.arg("oy")).toInt();
-  if(Server.arg("px") != NULL)  joyX = (Server.arg("px")).toInt();
-  if(Server.arg("py") != NULL)  joyY = (Server.arg("py")).toInt();
+}
+
+void joyEvent() {
+  if(Server.arg("px") != NULL)  joyX = (Server.arg("px")).toInt() - originX;
+  if(Server.arg("py") != NULL)  joyY = (Server.arg("py")).toInt() - originY;
+}
+
+void leftEvent() {
   if(Server.arg("l") != NULL)  turnL = (Server.arg("l")).toInt();
+}
+
+void rightEvent() {
   if(Server.arg("r") != NULL)  turnR = (Server.arg("r")).toInt();
-
-  // 原点設定時にはロボットを動かさない(誤動作するので)
-  if(Server.arg("ox") != NULL || Server.arg("oy") != NULL) return;
-  
-  if (turnL) {
-    turnRobot(true);
-  }
-  else if (turnR) {
-    turnRobot(false);
-  }
-  else {
-    moveRobot(joyX - originX, joyY - originY);
-  }
-  Server.send(200, "text/plane", "");
-}
-
-void moveRobot(int x, int y) {
-  Serial.printf("relX = %d\n", x);
-  Serial.printf("relY = %d\n", y);
-
-  motor(RIGHT_REAR_1, RIGHT_REAR_2, (x + y)/4);
-  motor(RIGHT_FRONT_1, RIGHT_FRONT_2, (y - x)/4);
-  motor(LEFT_REAR_1, LEFT_REAR_2, (y - x)/4);
-  motor(LEFT_FRONT_1, LEFT_FRONT_2, (x + y)/4);
-}
-void turnRobot(boolean left) {
-  Serial.printf("left = %d\n", left);
-
-  //Todo left値のtrue/falseにあわせて定速回転するだけ
-  
-  motor(RIGHT_REAR_1, RIGHT_REAR_2, left ? 255 : -255);
-  motor(RIGHT_FRONT_1, RIGHT_FRONT_2, left ? 255 : -255);
-  motor(LEFT_REAR_1, LEFT_REAR_2, left ? -255 : 255);
-  motor(LEFT_FRONT_1, LEFT_FRONT_2, left ? -255 : 255);
 }
 
 // ピンを配列として読めるようにしたほうが便利
-void motor(int 1pin, int 2pin, int power){
+void motor(int *pin, int power){
   if(power > 0){
-    analogWrite(1pin,  power);
-    analogWrite(2pin,  0);
+    analogWrite(pin[0],  power);
+    analogWrite(pin[1],  0);
   }
   else{
-    analogWrite(1pin,  0);
-    analogWrite(2pin, -1*power);
+    analogWrite(pin[0],  0);
+    analogWrite(pin[1], -1*power);
   }
 }
 
@@ -134,32 +103,60 @@ void setup() {
   }
   WiFi.softAPIP();
 
-  Server.on("/", handleRoot);
-  Server.on("/setLED", handlePWM);
+  //Server.on("/", handleRoot);
+  Server.on("/joyStart", joyStart);
+  Server.on("/joyEvent", joyEvent);
+  Server.on("/leftEvent", leftEvent);
+  Server.on("/rightEvent", rightEvent);
   Server.onNotFound(handleWebRequests);
   Server.begin();
 }
 
+int t = 0;
+
 void loop() {
-  Server.handleClient();
+  // さすがに細かく分けると重すぎることがわかった
+  // 十字+X字配置にして前進・後進・左右・斜め移動を行う
+
+  // 本当はpub-sub通信もどきを実装できればいけるんだと思う
+  if(t > 1000) {
+    ts = micros();
+    Server.handleClient();
+    te = micros();
+    Serial.printf("%d ms\n", te-ts);
+    t = 0;
+  }
+  
+   if(t % 10  == 0){
+      if (turnL) {
+      motor(RIGHT_REAR, -255);
+      motor(RIGHT_FRONT, -255);
+      motor(LEFT_REAR, -255);
+      motor(LEFT_FRONT, -255);
+    }
+    else if (turnR) {
+      motor(RIGHT_REAR, 255);
+      motor(RIGHT_FRONT, 255);
+      motor(LEFT_REAR, 255);
+      motor(LEFT_FRONT, 255);
+    }
+    else{
+      motor(RIGHT_REAR, (joyX + joyY)/4);
+      motor(RIGHT_FRONT, (joyY - joyX)/4);
+      motor(LEFT_REAR, (joyX - joyY)/4);
+      motor(LEFT_FRONT, (-1*joyX - joyY)/4);
+    }
+  }
+  t++;
 }
 
 bool loadFromSpiffs(String path) {
   String dataType = "text/plain";
   if (path.endsWith("/")) path += "index.htm";
-
   if (path.endsWith(".src")) path = path.substring(0, path.lastIndexOf("."));
   else if (path.endsWith(".html")) dataType = "text/html";
-  else if (path.endsWith(".htm")) dataType = "text/html";
   else if (path.endsWith(".css")) dataType = "text/css";
   else if (path.endsWith(".js")) dataType = "application/javascript";
-  else if (path.endsWith(".png")) dataType = "image/png";
-  else if (path.endsWith(".gif")) dataType = "image/gif";
-  else if (path.endsWith(".jpg")) dataType = "image/jpeg";
-  else if (path.endsWith(".ico")) dataType = "image/x-icon";
-  else if (path.endsWith(".xml")) dataType = "text/xml";
-  else if (path.endsWith(".pdf")) dataType = "application/pdf";
-  else if (path.endsWith(".zip")) dataType = "application/zip";
   File dataFile = SPIFFS.open(path.c_str(), "r");
   if (Server.hasArg("download")) dataType = "application/octet-stream";
   if (Server.streamFile(dataFile, dataType) != dataFile.size()) {
